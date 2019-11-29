989144e7c1bef98cb81b79b5a6f39ca7866712e9
jvlflame <jli141928@gmail.com>
2019-11-29 13:35:29 -0800

    Fix directory casing reference for linux compatibility

--- Javinizer/Javinizer.psm1
+++ Javinizer/Javinizer.psm1
@@ -1,6 +1,6 @@
 # Dot source public/private functions
-$public = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'public/*.ps1')  -Recurse -ErrorAction Stop)
-$private = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'private/*.ps1') -Recurse -ErrorAction Stop)
+$public = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Public/*.ps1')  -Recurse -ErrorAction Stop)
+$private = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Private/*.ps1') -Recurse -ErrorAction Stop)
 foreach ($import in @($public + $private)) {
     try {
         . $import.FullName
