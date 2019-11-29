9e59db91b5f89ca761de2d464e4734e8e38b4679
jvlflame <jli141928@gmail.com>
2019-11-29 12:53:09 -0800

    Update cloudscraper useragent string

--- Javinizer/cfscraper.py
+++ Javinizer/cfscraper.py
@@ -1,8 +1,8 @@
 import cloudscraper
 import sys
 
-cookie_value, user_agent = cloudscraper.create_scraper(
-    browser={'browser': 'chrome', 'desktop': True}).get_cookie_string(sys.argv[1])
+cookie_value, user_agent = cloudscraper.get_cookie_string(
+    sys.argv[1], browser={'browser': 'chrome', 'mobile': False})
 #cookie_value, user_agent = cloudscraper.get_cookie_string(sys.argv[1])
 
 print('{}\n{}'.format(cookie_value, user_agent))
