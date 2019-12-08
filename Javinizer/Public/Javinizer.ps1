3c7f4df78b09ae79230d6a688fb1f6567fceb934
jvlflame <jli141928@gmail.com>
2019-12-08 13:19:43 -0800

    Add comment based help and examples

--- Javinizer/Public/Javinizer.ps1
+++ Javinizer/Public/Javinizer.ps1
@@ -1,4 +1,91 @@
 function Javinizer {
+
+    <#
+    .SYNOPSIS
+        A command-line based tool to scrape and sort your local Japanese Adult Video (JAV) files
+
+    .DESCRIPTION
+        Javinizer is used to pull data from online data sources such as JAVLibrary, DMM, and R18 to aggregate data into a CMS (Plex,Emby,Jellyfin) parseable format.
+
+    .PARAMETER Find
+        The find parameter will output a list-formatted data output from the data sources specified using a movie ID, file path, or URL.
+
+    .PARAMETER Aggregated
+        The aggregated parameter will create an aggregated list-formatted data output from the data sources specified as well as metadata priorities in your settings.ini file.
+
+    .PARAMETER Path
+        The path parameter sets the file or directory path that Javinizer will search and sort files in.
+
+    .PARAMETER DestinationPath
+        The destinationpath parameter sets the directory path that Javinizer will send sorted files to.
+
+    .PARAMETER Url
+        The url parameter allows you to set direct URLs to JAVLibrary, DMM, and R18 data sources to scrape a video from in comma-separated-format (url1,url2,url3).
+
+    .PARAMETER Apply
+        The apply parameter allows you to automatically begin your sort using settings specified in your settings.ini file.
+
+    .PARAMETER Multi
+        The multi parameter will perform your sort using multiple concurrent threads with a throttle limit of (1-5) set in your settings.ini file.
+
+    .PARAMETER R18
+        The r18 parameter allows you to set your data source of R18 to true.
+
+    .PARAMETER Dmm
+        The dmm parameter allows you to set your data source of DMM to true.
+
+    .PARAMETER Javlibrary
+        The javlibrary parameter allows you to set your data source of JAVLibrary to true.
+
+    .PARAMETER Force
+        The force parameter will attempt to force any new sorted files to be overwritten if it already exists.
+
+    .PARAMETER ScriptRoot
+        The scriptroot parameter sets the default Javinizer module directory. This should not be touched.
+
+    .EXAMPLE
+        PS> Javinizer -Apply -Multi
+
+        Description
+        -----------
+        Performs a multi-threaded sort on your directories with settings specified in your settings.ini file.
+
+    .EXAMPLE
+        PS> Javinizer -Path C:\Downloads -DestinationPath C:\Downloads\Sorted
+
+        Description
+        -----------
+        Performs a single-threaded sort on your specified Path with other settings specified in your settings.ini file.
+
+    .EXAMPLE
+        PS> Javinizer -Path 'C:\Downloads\Jav\snis-620.mp4' -DestinationPath C:\Downloads\JAV\Sorted\' -Url 'http://www.javlibrary.com/en/?v=javlilljyy,https://www.r18.com/videos/vod/movies/detail/-/id=snis00620/?i3_ref=search&i3_ord=1,https://www.dmm.co.jp/digital/videoa/-/detail/=/cid=snis00620/?i3_ref=search&i3_ord=4'
+
+        Description
+        -----------
+        Performs a single-threaded sort on your specified file using direct URLs to match the file.
+
+    .EXAMPLE
+        PS> Javinizer -Find SNIS-420
+
+        Description
+        -----------
+        Performs a console search of SNIS-420 for all data sources specified in your settings.ini file
+
+    .EXAMPLE
+        PS> Javinizer -Find SNIS-420 -R18 -DMM -Aggregated
+
+        Description
+        -----------
+        Performs a console search of SNIS-420 for R18 and DMM and aggregates output to your settings specified in your settings.inifile.
+
+    .EXAMPLE
+        PS> Javinizer -Find 'https://www.r18.com/videos/vod/movies/detail/-/id=pred00200/?dmmref=video.movies.new&i3_ref=list&i3_ord=2'
+
+        Description
+        -----------
+        Performs a console search of PRED-200 using a direct url.
+    #>
+
     [CmdletBinding(DefaultParameterSetName = 'Path')]
     param (
         [Parameter(ParameterSetName = 'Info', Mandatory = $true, Position = 0)]
@@ -15,8 +102,6 @@ function Javinizer {
         [Parameter(ParameterSetName = 'Path', Mandatory = $false)]
         [Alias('u')]
         [string]$Url,
-        [Parameter(ParameterSetName = 'Path', Mandatory = $false)]
-        [switch]$PassThru,
         [Parameter(ParameterSetName = 'Path', Mandatory = $false)]
         [Alias('a')]
         [switch]$Apply,
