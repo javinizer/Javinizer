ec4f97998ccaf585c81b0283e86ac5e5096c6bf0
jvlflame <jli141928@gmail.com>
2019-12-11 21:52:54 -0800

    Add confirm message prompt function

---
+++ src/Javinizer/Private/Display-ConfirmMessage.ps1
@@ -0,0 +1,27 @@
+function Display-ConfirmMessage {
+    [CmdletBinding()]
+    param(
+        [string]$Message,
+        [string]$Default
+    )
+
+    process {
+        $Default = $Default.ToUpper()
+        Write-Host "$Message"
+        $selection = Read-Host "[Y] Yes  [N] No  (Default is ""$Default"")"
+        do {
+            if ($selection -eq '' -or $null -eq $selection) {
+                $selection = $Default
+            } elseif ($selection -eq 'y' -or $selection -eq 'yes') {
+                $selection = 'y'
+            } elseif ($selection -eq 'n' -or $selection -eq 'no') {
+                $selection = 'n'
+            } else {
+                Write-Warning "Your selection [$selection] is invalid. Please enter a valid selection."
+                $selection = Read-Host "[Y] Yes  [N] No  (Default is ""$Default"")"
+            }
+        } until ($selection -eq 'y' -or $selection -eq 'n')
+
+        Write-Output $selection
+    }
+}
