a48b4bae8fc229974c13ebc555bc45213e265339
jvlflame <jli141928@gmail.com>
2020-09-01 01:21:04 -0700

    Removed logging module

--- src/Javinizer/External/Logging/private/Get-LevelNumber.ps1
+++
@@ -1,9 +0,0 @@
-function Get-LevelNumber {
-    [CmdletBinding()]
-    param(
-        $Level
-    )
-    if ($Level -is [int] -and $Level -in $Script:LevelNames.Keys) {return $Level}
-    elseif ([string] $Level -eq $Level -and $Level -in $Script:LevelNames.Keys) {return $Script:LevelNames[$Level]}
-    else {throw ('Level not a valid integer or a valid string: {0}' -f $Level)}
-}
--- src/Javinizer/External/Logging/private/Get-LevelsName.ps1
+++
@@ -1,6 +0,0 @@
-Function Get-LevelsName {
-    [CmdletBinding()]
-    param()
-
-    return $Script:LevelNames.Keys | Where-Object {$_ -isnot [int]} | Sort-Object
-}
