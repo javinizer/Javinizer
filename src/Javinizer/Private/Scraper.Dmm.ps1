918fe8d50b6e172e150bdb174dde800e2230c53a
seeyabye <36906720+seeyabye@users.noreply.github.com>
2020-09-21 12:27:28 +0900

    Added DMM Trailer (#90)
    * Added DMM Trailer
    
    * Revised scraping method for DMM trailer
    
    * Fix for en
    
    * Add session to trailerurl
    
    * Add session to trailerurl

--- src/Javinizer/Private/Scraper.Dmm.ps1
+++ src/Javinizer/Private/Scraper.Dmm.ps1
@@ -389,25 +389,29 @@ function Get-DmmScreenshotUrl {
     }
 }
 
-# ! Unable to get trailer url from HTTP from main DMM video page
-<# function Get-DmmTrailerUrl {
+function Get-DmmTrailerUrl {
     param (
-        [Object]$Webrequest
+        [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
+        [Object]$Webrequest,
+
+        [Parameter()]
+        [Object]$Session
     )
 
-    begin {
-        $trailerUrl = @()
-    }
-
     process {
-        $trailerHtml = $Webrequest.Content -split '\n'
-        $trailerHtml = $trailerHtml | Select-String -Pattern 'https:\/\/cc3001\.dmm\.co\.jp\/litevideo\/freepv' -AllMatches
-
-        foreach ($trailer in $trailerHtml) {
-            $trailer = (($trailer -split '"')[1] -split '"')[0]
-            $trailerUrl += $trailer
+        $trailerUrl = @()
+        $iFrameUrl = 'https://www.dmm.co.jp' + ($Webrequest.Content | Select-String -Pattern "onclick.+sampleplay\('([^']+)'\)").Matches.Groups[1].Value
+        try {
+            $trailerPageUrl = ((Invoke-WebRequest -Uri $iFrameUrl -WebSession $session -Verbose:$false).Content | Select-String -Pattern 'src="([^"]+)"').Matches.Groups[1].Value -replace '/en', ''
+            $trailerUrl = ((Invoke-WebRequest -Uri $trailerPageUrl -Verbose:$false).Content | Select-String -Pattern '\\/\\/cc3001\.dmm\.co\.jp\\/litevideo\\/freepv[^"]+').Matches.Groups[0].Value -replace '\\', ''
+            if ($trailerUrl -match '^//') {
+                $trailerUrl = "https:$trailerUrl"
+            }
+        } catch {
+            return
         }
 
-        Write-Output $trailerUrl
+        Write-Output "https:$trailerUrl"
     }
-} #>
+}
+
