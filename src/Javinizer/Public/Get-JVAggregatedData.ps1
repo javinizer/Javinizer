baa59b1612d35810c1ab86ef91d4bdaa50ac57f6
jvlflame <jli141928@gmail.com>
2020-09-22 20:20:13 -0700

    Fix variable name for unknownactress

--- src/Javinizer/Public/Get-JVAggregatedData.ps1
+++ src/Javinizer/Public/Get-JVAggregatedData.ps1
@@ -163,7 +163,7 @@ function Get-JVAggregatedData {
             $ActressLanguageJa = $Settings.'sort.metadata.nfo.actresslanguageja'
             $ThumbCsvAutoAdd = $Settings.'sort.metadata.thumbcsv.autoadd'
             $FirstNameOrder = $Settings.'sort.metadata.nfo.firstnameorder'
-            $AddUnknownActress = $Settings.'sort.metadata.nfo.unknownactress'
+            $UnknownActress = $Settings.'sort.metadata.nfo.unknownactress'
             if ($Settings.'location.genrecsv' -ne '') {
                 $GenreCsvPath = $Settings.'location.genrecsv'
             }
@@ -450,7 +450,7 @@ function Get-JVAggregatedData {
             }
         }
 
-        if ($AddUnknownActress) {
+        if ($UnknownActress) {
             if ($null -eq $aggregatedDataObject.Actress) {
                 $aggregatedDataObject.Actress += [PSCustomObject]@{
                     LastName     = $null
