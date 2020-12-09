13dd727b6a005cf9bf98a2c43e2fedfaacc0ffa5
jvlflame <jli141928@gmail.com>
2020-12-08 16:50:08 -0800

    Fix unicode errors on translation with asian languages (#152)

--- src/Javinizer/Private/Get-TranslatedString.ps1
+++ src/Javinizer/Private/Get-TranslatedString.ps1
@@ -11,21 +11,19 @@ function Get-TranslatedString {
     process {
         $translatePath = Join-Path -Path ((Get-Item $PSScriptRoot).Parent) -ChildPath 'translate.py'
 
-        if ($String -eq $null -or $String -eq '') {
-            # Do not translate if empty
-        } else {
+        if ($null -ne $String -and $String -ne '') {
             try {
-                $tempFile = New-TemporaryFile
                 if ([System.Environment]::OSVersion.Platform -eq 'Win32NT') {
-                    python $translatePath $String $Language | Set-Content $tempFile -Encoding oem
+                    $tempFile = python $translatePath $String $Language
                 } elseif ([System.Environment]::OSVersion.Platform -eq 'Unix') {
-                    python3 $translatePath $String $Language | Set-Content $tempFile -Encoding oem
+                    $tempFile = python3 $translatePath $String $Language
                 }
                 $translatedString = Get-Content -Path $tempFile -Encoding utf8 -Raw
             } finally {
                 Remove-Item -Path $tempFile -ErrorAction SilentlyContinue
             }
         }
+
         Write-Output $translatedString
     }
 }
--- src/Javinizer/translate.py
+++ src/Javinizer/translate.py
@@ -1,8 +1,17 @@
 from google_trans_new import google_translator
 import sys
+import tempfile
+import os
 
 google_translator = google_translator()
 translation = google_translator.translate(sys.argv[1], lang_tgt=sys.argv[2])
 
 text = translation.encode('utf8')
-sys.stdout.buffer.write(text)
+
+# Write the translated text to a temporary file to bypass encoding issues when redirecting the text to PowerShell
+new_file, filename = tempfile.mkstemp()
+os.write(new_file, text)
+os.close(new_file)
+
+# Return the path to the temporary file to read it from PowerShell
+print(filename)
