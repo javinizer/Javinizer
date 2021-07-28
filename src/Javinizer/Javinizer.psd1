e466e7faf1aa509377ccb1d580c230a7f1ca73c9
jvlflame <jli141928@gmail.com>
2021-07-28 08:11:05 -0700

    Update for 2.5.4

--- src/Javinizer/Javinizer.psd1
+++ src/Javinizer/Javinizer.psd1
@@ -13,7 +13,7 @@
 
     # Version number of this module.
 
-    ModuleVersion     = '2.5.3'
+    ModuleVersion     = '2.5.4'
 
     # Supported PSEditions
     # CompatiblePSEditions = @('Core')
--- src/Javinizer/Universal/Repository/javinizergui.ps1
+++ src/Javinizer/Universal/Repository/javinizergui.ps1
@@ -1,4 +1,4 @@
-﻿$cache:guiVersion = '2.5.3-1'
+﻿$cache:guiVersion = '2.5.4-1'
 
 # Define Javinizer module file paths
 $cache:modulePath = (Get-InstalledModule -Name Javinizer).InstalledLocation
